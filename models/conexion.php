2a1c130824d438bb6438726c0699338487f953bc
Alexander <juanperzezss@gmail.com>
2024-10-28 14:54:50 -0500

    Subida inicial del proyecto

--- models/conexion.php
+++ models/conexion.php
@@ -1,8 +1,18 @@
 <?php
-    class conexion {
-        public function conectar(){
-            $conn= new PDO("mysql:host=localhost;dbname=cuarto",'root','');
+class conexion {
+    public function conectar() {
+        $host = getenv('DB_HOST');
+        $dbname = getenv('DB_NAME');
+        $user = getenv('DB_USER');
+        $pass = getenv('DB_PASS');
+
+        try {
+            $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
         }
     }
+}
 ?>
